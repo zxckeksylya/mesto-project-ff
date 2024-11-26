3d467d1659829b68daa619464f56d070837b1ced
Egor Gridin <egorka.gridin@gmail.com>
2024-11-26 15:32:08 +0300

    исправлены замечания ревьювера

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,10 +1,12 @@
-import modal from "./modal";
-
-const cardTemplate = document.querySelector("#card-template").content;
-
-function Card(card, removeCalback) {
+function createCard({
+  card,
+  calbacks: {
+    removeCalback = () => {},
+    cardTemplate,
+    openModalImageCallBack = () => {},
+  },
+}) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-
   const likeButton = cardElement.querySelector(".card__like-button");
 
   cardElement.querySelector(".card__title").textContent = card.name;
@@ -12,22 +14,21 @@ function Card(card, removeCalback) {
   const cardImage = cardElement.querySelector(".card__image");
   cardImage.setAttribute("src", card.link);
   cardImage.setAttribute("alt", card.name);
-  const popup = modal.imageModal(card);
-  cardImage.addEventListener("click", popup.open);
-
-  const like = function (event) {
-    event.target.classList.toggle("card__like-button_is-active");
-  };
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  const like = (event) => {
+    event.target.classList.toggle("card__like-button_is-active");
+  };
 
   const removeCard = (event) => {
     deleteButton.removeEventListener("click", removeCard);
     likeButton.removeEventListener("click", like);
-    cardImage.removeEventListener("click", popup.open);
+    cardImage.removeEventListener("click", openModalImageCallBack);
     removeCalback(event);
   };
 
+  cardImage.addEventListener("click", openModalImageCallBack);
   deleteButton.addEventListener("click", removeCard);
   likeButton.addEventListener("click", like);
 
@@ -47,6 +48,6 @@ export const removeCard = (event) => {
   cardItem.remove();
 };
 export default {
+  createCard,
   removeCard,
-  Card,
 };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,229 @@
 import "../pages/index.css";
 import card from "./card";
-import modal from "./modal";
 import ininitialCards from "./cards";
+import createModal from "./modal";
 
 const cardsContainer = document.querySelector(".places__list");
 const profileAddButton = document.querySelector(".profile__add-button");
 const profileEditButton = document.querySelector(".profile__edit-button");
 
+const cardTemplate = document.querySelector("#card-template").content;
+
 const editFormElement = document.forms["edit-profile"];
 const cardFormElement = document.forms["new-place"];
 
-const editForm = modal.editFormModal(editFormElement);
-const cardForm = modal.cardFormModal(cardFormElement);
+const popupTypeImage = document.querySelector(".popup_type_image");
+const cardImage = document.querySelector(".popup__image");
+const cardCaption = document.querySelector(".popup__caption");
 
-profileEditButton.addEventListener("click", editForm.open);
-profileAddButton.addEventListener("click", cardForm.open);
+const popupTypeNewCard = document.querySelector(".popup_type_new-card");
+const popupTypeEdit = document.querySelector(".popup_type_edit");
+
+const title = document.querySelector(".profile__title");
+const description = document.querySelector(".profile__description");
+
+function createImageModal(obj) {
+  const modal = createModal(popupTypeImage);
+
+  const open = () => {
+    cardImage.alt = obj.name;
+    cardImage.src = obj.link;
+    cardCaption.textContent = obj.name;
+    modal.open();
+  };
+
+  const close = () => {
+    modal.close();
+  };
+
+  return {
+    open,
+    close,
+  };
+}
+
+function Control(control, validateRules) {
+  const getValue = () => {
+    return control.value;
+  };
+
+  const setValue = (value) => {
+    control.value = value;
+  };
+
+  const validate = () => {
+    return validateRules.reduce((prev, curr) => {
+      if (prev == false) {
+        return false;
+      }
+
+      return curr(control.value);
+    }, true);
+  };
+
+  return {
+    getValue,
+    setValue,
+    validate,
+  };
+}
+
+const required = (value) => {
+  if (value !== "") {
+    return true;
+  }
+  return false;
+};
+
+function Form({ form, controls, submitCallback = () => {} }) {
+  const validate = () => {
+    const status = Object.values(controls).reduce((prev, curr) => {
+      if (prev === false) {
+        return false;
+      }
+      return curr.validate();
+    }, true);
+
+    return status;
+  };
+
+  const submit = (evt) => {
+    evt.preventDefault();
+    if (validate) {
+      submitCallback();
+      reset();
+    } else {
+    }
+  };
+
+  const reset = () => {
+    form.reset();
+  };
+
+  return {
+    submit,
+    reset,
+  };
+}
+
+function editFormModal({ editFormElement, editObj: { title, description } }) {
+  const modalProfile = createModal(popupTypeEdit, closeCalback);
+
+  const controls = {
+    name: Control(editFormElement.elements["name"], [required]),
+    description: Control(editFormElement.elements["description"], [required]),
+  };
+
+  const submitCallback = () => {
+    title.textContent = controls.name.getValue();
+    description.textContent = controls.description.getValue();
+    modalProfile.close();
+  };
+
+  const form = Form({
+    form: editFormElement,
+    controls,
+    submitCallback,
+  });
+
+  const closeCalback = () => {
+    form.reset();
+    editFormElement.removeEventListener("submit", form.submit);
+    editFormElement.removeEventListener("reset", form.reset);
+  };
+
+  const open = () => {
+    controls.name.setValue(title.textContent);
+    controls.description.setValue(description.textContent);
+    editFormElement.addEventListener("submit", form.submit);
+    editFormElement.addEventListener("reset", form.reset);
+
+    modalProfile.open();
+  };
+
+  const close = () => {
+    modalProfile.close(() => {});
+  };
+
+  return {
+    open,
+    close,
+  };
+}
+
+function cardFormModal(cardFormElement) {
+  const controls = {
+    "place-name": Control(cardFormElement.elements["place-name"], [required]),
+    link: Control(cardFormElement.elements["link"], [required]),
+  };
+
+  const submitCallback = () => {
+    const cardObj = {
+      name: controls["place-name"].getValue(),
+      link: controls.link.getValue(),
+    };
+    const newCard = card.createCard({
+      card: cardObj,
+      calbacks: {
+        removeCalback: card.removeCard,
+        cardTemplate,
+        openModalImageCallBack: createImageModal(cardObj).open,
+      },
+    });
+
+    cardsContainer.prepend(newCard.getElement());
+    modal.close();
+  };
+
+  const form = Form({ form: cardFormElement, controls, submitCallback });
+
+  const modal = createModal(popupTypeNewCard, closeCalback);
+
+  const closeCalback = () => {
+    form.reset();
+    cardFormElement.removeEventListener("submit", form.submit);
+    cardFormElement.removeEventListener("reset", form.reset);
+  };
+
+  const open = () => {
+    modal.open();
+    cardFormElement.addEventListener("submit", form.submit);
+    cardFormElement.addEventListener("reset", form.reset);
+  };
+
+  const close = () => {
+    modal.close();
+  };
+
+  return {
+    open,
+    close,
+  };
+}
 
 function generatePage() {
   ininitialCards.forEach((item) => {
-    cardsContainer.append(card.Card(item, card.removeCard).getElement());
+    const calbacks = {
+      removeCalback: card.removeCard,
+      cardTemplate,
+      openModalImageCallBack: createImageModal(item).open,
+    };
+
+    const newCard = card.createCard({
+      card: item,
+      calbacks,
+    });
+    cardsContainer.append(newCard.getElement());
   });
 }
+
+const editForm = editFormModal({
+  editFormElement,
+  editObj: { title, description },
+});
+const cardForm = cardFormModal(cardFormElement);
+
+profileEditButton.addEventListener("click", editForm.open);
+profileAddButton.addEventListener("click", cardForm.open);
 
 generatePage();
